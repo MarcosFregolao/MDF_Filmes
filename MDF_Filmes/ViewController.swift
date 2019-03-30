80918b5b2bc218d0917827b3b1e3877a8066be08
PUCPR <pucpr@ladmac0123.local>
2019-03-29 21:48:08 -0300

    Criação de meu primeiro APP consumindo JSON.

--- MDF_Filmes/ViewController.swift
+++ MDF_Filmes/ViewController.swift
@@ -1,20 +1,45 @@
-//
-//  ViewController.swift
-//  MDF_Filmes
-//
-//  Created by PUCPR on 29/03/19.
-//  Copyright © 2019 PUCPR. All rights reserved.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
-}
-
+            import UIKit
+            import Alamofire
+            import SwiftyJSON
+            
+            class ViewController: UIViewController {
+                
+                
+                @IBOutlet weak var lblTexto: UILabel!
+                
+                @IBOutlet weak var imgImagem: UIImageView!
+                
+                
+                override func viewDidLoad() {
+                    super.viewDidLoad()
+                    
+                    let urlBase = "https://api.themoviedb.org/3/movie/329996?api_key="
+                    let imgBase = "https://image.tmdb.org/t/p/w500"
+                    let key = "1213e00ad70b3dada7681f1b85e44c22"
+                    
+                    let url = "\(urlBase)\(key)"
+                    
+                    Alamofire.request(url).responseJSON { (response) in
+                        //essa função é assíncrona, então tudo o que é pra ser feito
+                        //após a requisição deve ser colocado nesse closure
+                        //e não na linha de baixo, pq a execução não é linear
+                        //justamente por depender da resposta do servidor
+                        
+                        //site pra olhar o json mais claramente: http://jsoneditoronline.org
+                        
+                        if response.result.isSuccess {
+                            let json = JSON(response.result.value!)
+                            self.lblTexto.text = json["title"].stringValue
+                            let imgPath = "\(imgBase)\(json["poster_path"].stringValue)"
+                            let urlImage = URL(string: imgPath)
+                            let bytes = try? Data(contentsOf: urlImage!)
+                            let poster = UIImage(data: bytes!)
+                            self.imgImagem.image = poster
+                        }
+                        
+                        //colocar no info.plist as informações sobre fazer
+                        //carregamento arbitrário OU especificar de onde será
+                        //feito o carregamento (Allow Arbitrary Loads)
+                    }
+                }
+            }
